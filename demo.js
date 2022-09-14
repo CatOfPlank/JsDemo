ccc51ebc7f3cffaf18e7e4abde3971d1fa7190be
Haibara <2586225191@qq.com>
2022-09-14 21:04:35 +0800

    add license

--- demo.js
+++ demo.js
@@ -6,6 +6,7 @@
 // @author       Haibara
 // @match        https://ntp.msn.cn/edge/ntp?locale=zh-CN&title=%E6%96%B0%E5%BB%BA%E6%A0%87%E7%AD%BE%E9%A1%B5&dsp=0&sp=%E5%BF%85%E5%BA%94
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=msn.cn
+// @license      AGPL-3.0
 // @grant        none
 // ==/UserScript==
 
