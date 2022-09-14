019793184446831003fca5d91a3a968b33a34932
Haibara <2586225191@qq.com>
2022-09-14 19:35:07 +0800

    first version

---
+++ demo.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         hello world
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Haibara
+// @match        https://ntp.msn.cn/edge/ntp?locale=zh-CN&title=%E6%96%B0%E5%BB%BA%E6%A0%87%E7%AD%BE%E9%A1%B5&dsp=0&sp=%E5%BF%85%E5%BA%94
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=msn.cn
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
