10978b46091d8419809656aaff1c796d27c0779c
Haibara <2586225191@qq.com>
2022-09-14 21:57:10 +0800

    create name

--- demo.js
+++ demo.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         hello world
+// @name         Hello world
 // @namespace    http://tampermonkey.net/
 // @version      0.2
 // @description  try to take over the world!
