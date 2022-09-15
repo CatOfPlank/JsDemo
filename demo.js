49afb8ed020101b0778c46e81408429241149682
Haibara <2586225191@qq.com>
2022-09-15 19:22:00 +0800

    version 0.3

--- demo.js
+++ demo.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto Log
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       Haibara
 // @match        https://www.firebbs.cn/index.php
